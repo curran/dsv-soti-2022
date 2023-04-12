ac2c32089bb1569af8edcbb946cbb27c24e8a4de
Curran <curran.kelleher@gmail.com>
2023-04-12 02:25:02 -0400

    Compress data

--- processData.js
+++ processData.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { tsvParse, csvFormat } from 'd3-dsv';
+import { tsvParse, csvParse, csvFormat } from 'd3-dsv';
 
 // This script processes the dirty raw data files
 // into clean usable files.
@@ -23,4 +23,40 @@ const processDictionary = () => {
   fs.writeFileSync('./data/dictionary2022main.csv', csvFormat(data));
 };
 
+const processMain = () => {
+  // This file is much too large at 8MB.
+  // The following logic compresses the file by creating a
+  // data dictionary mapping numbers to strings.
+  const rawCSV = fs.readFileSync('./data/raw/data2022main.csv', 'utf-8');
+  const data = csvParse(rawCSV);
+
+  // This will be the new key that we store.
+  let i = 0;
+
+  const valueToKey = new Map();
+
+  for (const d of data) {
+    for (const column of data.columns) {
+      const value = d[column];
+      let key = valueToKey.get(value);
+
+      // If this value does is not yet indexed,
+      if (!key) {
+        // mint a new key for it and index it.
+        key = i++ + '';
+        valueToKey.set(value, key);
+      }
+      // Replace the long string with the short key.
+      d[column] = key;
+    }
+  }
+
+  const values = Array.from(valueToKey.entries()).map(([value, key]) => ({ key, value }));
+
+  // Generate a CSV file that's directly usable in client-side code.
+  fs.writeFileSync('./data/main.csv', csvFormat(data));
+  fs.writeFileSync('./data/mainValues.csv', csvFormat(values));
+};
+
 processDictionary();
+processMain();
